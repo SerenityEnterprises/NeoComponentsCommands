a696aef651870c53cef4e56c9d3345eca9ba43e0
half cambodian hacker man <anthonyjoshuasom@gmail.com>
2017-11-16 22:33:12 +0000

    Fix default parsers for java.lang boxed types

--- src/main/kotlin/host/serenity/neo/components/commands/DefaultParsers.kt
+++ src/main/kotlin/host/serenity/neo/components/commands/DefaultParsers.kt
@@ -11,4 +11,14 @@ fun addDefaultParsers(parserRegistry: MutableMap<Class<*>, ArgumentParser<*>>) {
     parserRegistry.put(Short::class.java, ShortParser())
     parserRegistry.put(Long::class.java, LongParser())
     parserRegistry.put(Char::class.java, CharParser())
+
+
+    // Java interop
+    parserRegistry.put(java.lang.Integer::class.java, parserRegistry[Int::class.java]!!)
+    parserRegistry.put(java.lang.Float::class.java, parserRegistry[Float::class.java]!!)
+    parserRegistry.put(java.lang.Double::class.java, parserRegistry[Double::class.java]!!)
+    parserRegistry.put(java.lang.Short::class.java, parserRegistry[Short::class.java]!!)
+    parserRegistry.put(java.lang.Long::class.java, parserRegistry[Long::class.java]!!)
+    parserRegistry.put(java.lang.Character::class.java, parserRegistry[Char::class.java]!!)
+    // parserRegistry.put(java.lang.String::class.java, parserRegistry[String::class.java]!!)
 }
